a0c498cf88b66eb071715d79d11a0d4ba72b7f5e
ivancalarde1301 <ivan.calarde1301@gmail.com>
2022-07-18 23:11:52 +0800

    public string Configuration

--- PDFResumeCreator/PDFResumeCreator/Form1.cs
+++ PDFResumeCreator/PDFResumeCreator/Form1.cs
@@ -16,8 +16,46 @@ using iTextSharp.text.pdf;
 
 namespace ResumeCreator
 {
+    public class Resume
+    {
+        public string[] PersonalInformation { get; set; }
+        public string[] Address { get; set; }
+        public string CareerObjective { get; set; }
+        public string[] WorkExperience { get; set; }
+        public string Education { get; set; }
+    }
     public partial class ResumeCreator : Form
     {
+        public class Resume
+        {
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public string Position { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public string URL { get; set; }
+            public string UBL { get; set; }
+            public string Street { get; set; }
+            public string Barangay { get; set; }
+            public string ZipCode { get; set; }
+            public string City { get; set; }
+            public string Province { get; set; }
+            public string Region { get; set; }
+            public string CareerObjective { get; set; }
+            public string WorkExperience1Work { get; set; }
+            public string WorkExperience1Date { get; set; }
+            public string WorkExperience1Description { get; set; }
+            public string WorkExperience2Work { get; set; }
+            public string WorkExperience2Date { get; set; }
+            public string WorkExperience2Description { get; set; }
+            public string TertiaryEducation { get; set; }
+            public string Course { get; set; }
+            public string SecondaryEducation { get; set; }
+            public string Track { get; set; }
+            public string PrimaryEducation { get; set; }
+        }
+
         public ResumeCreator()
         {
             InitializeComponent();
@@ -34,14 +72,6 @@ namespace ResumeCreator
         private void buttonCreateResume_Click(object sender, EventArgs e)
         {
 
-        }
-
-        public class Resume
-        {
-            public string PersonalInformation { get; set; }
-            public string CareerObjective { get; set; }
-            public string WorkExperience { get; set; }
-            public string Education { get; set; }
         }
     }
 
