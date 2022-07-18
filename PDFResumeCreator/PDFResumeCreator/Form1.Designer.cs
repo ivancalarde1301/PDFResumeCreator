6973bcff008d152a397f3681807b1c09fcd821f5
ivancalarde1301 <ivan.calarde1301@gmail.com>
2022-07-18 13:38:01 +0800

    UI Configuration

--- PDFResumeCreator/PDFResumeCreator/Form1.Designer.cs
+++ PDFResumeCreator/PDFResumeCreator/Form1.Designer.cs
@@ -28,13 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelPDFResumeCreator = new System.Windows.Forms.Label();
+            this.labelPersonalInformation = new System.Windows.Forms.Label();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.labelMiddleName = new System.Windows.Forms.Label();
+            this.textBoxMiddleName = new System.Windows.Forms.TextBox();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.textBoxPosition = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelURL = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelPDFResumeCreator
+            // 
+            this.labelPDFResumeCreator.AutoSize = true;
+            this.labelPDFResumeCreator.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPDFResumeCreator.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelPDFResumeCreator.Location = new System.Drawing.Point(12, 9);
+            this.labelPDFResumeCreator.Name = "labelPDFResumeCreator";
+            this.labelPDFResumeCreator.Size = new System.Drawing.Size(314, 42);
+            this.labelPDFResumeCreator.TabIndex = 0;
+            this.labelPDFResumeCreator.Text = "PDF Resume Creator";
+            // 
+            // labelPersonalInformation
+            // 
+            this.labelPersonalInformation.AutoSize = true;
+            this.labelPersonalInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPersonalInformation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelPersonalInformation.Location = new System.Drawing.Point(15, 51);
+            this.labelPersonalInformation.Name = "labelPersonalInformation";
+            this.labelPersonalInformation.Size = new System.Drawing.Size(163, 20);
+            this.labelPersonalInformation.TabIndex = 1;
+            this.labelPersonalInformation.Text = "Personal Information";
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelFirstName.Location = new System.Drawing.Point(16, 81);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(72, 16);
+            this.labelFirstName.TabIndex = 2;
+            this.labelFirstName.Text = "First Name";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(114, 78);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(144, 22);
+            this.textBoxFirstName.TabIndex = 3;
+            // 
+            // labelMiddleName
+            // 
+            this.labelMiddleName.AutoSize = true;
+            this.labelMiddleName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelMiddleName.Location = new System.Drawing.Point(16, 107);
+            this.labelMiddleName.Name = "labelMiddleName";
+            this.labelMiddleName.Size = new System.Drawing.Size(88, 16);
+            this.labelMiddleName.TabIndex = 4;
+            this.labelMiddleName.Text = "Middle Name";
+            // 
+            // textBoxMiddleName
+            // 
+            this.textBoxMiddleName.Location = new System.Drawing.Point(114, 104);
+            this.textBoxMiddleName.Name = "textBoxMiddleName";
+            this.textBoxMiddleName.Size = new System.Drawing.Size(144, 22);
+            this.textBoxMiddleName.TabIndex = 5;
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelLastName.Location = new System.Drawing.Point(16, 133);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(72, 16);
+            this.labelLastName.TabIndex = 6;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(114, 130);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(144, 22);
+            this.textBoxLastName.TabIndex = 7;
+            // 
+            // labelPosition
+            // 
+            this.labelPosition.AutoSize = true;
+            this.labelPosition.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelPosition.Location = new System.Drawing.Point(16, 159);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(55, 16);
+            this.labelPosition.TabIndex = 8;
+            this.labelPosition.Text = "Position";
+            // 
+            // textBoxPosition
+            // 
+            this.textBoxPosition.Location = new System.Drawing.Point(114, 156);
+            this.textBoxPosition.Name = "textBoxPosition";
+            this.textBoxPosition.Size = new System.Drawing.Size(144, 22);
+            this.textBoxPosition.TabIndex = 9;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelEmail.Location = new System.Drawing.Point(16, 185);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(41, 16);
+            this.labelEmail.TabIndex = 10;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelPhone.Location = new System.Drawing.Point(16, 211);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(46, 16);
+            this.labelPhone.TabIndex = 11;
+            this.labelPhone.Text = "Phone";
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelURL.Location = new System.Drawing.Point(16, 237);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(34, 16);
+            this.labelURL.TabIndex = 12;
+            this.labelURL.Text = "URL";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelAddress.Location = new System.Drawing.Point(16, 263);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(58, 16);
+            this.labelAddress.TabIndex = 13;
+            this.labelAddress.Text = "Address";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(114, 182);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(144, 22);
+            this.textBoxEmail.TabIndex = 14;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(114, 208);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(144, 22);
+            this.textBoxPhone.TabIndex = 15;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(114, 234);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(144, 22);
+            this.textBoxURL.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelURL);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.textBoxPosition);
+            this.Controls.Add(this.labelPosition);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.textBoxMiddleName);
+            this.Controls.Add(this.labelMiddleName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.labelPersonalInformation);
+            this.Controls.Add(this.labelPDFResumeCreator);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Name = "Form1";
+            this.Text = "PDF Resume Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelPDFResumeCreator;
+        private System.Windows.Forms.Label labelPersonalInformation;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label labelMiddleName;
+        private System.Windows.Forms.TextBox textBoxMiddleName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.TextBox textBoxPosition;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxURL;
     }
 }
 
